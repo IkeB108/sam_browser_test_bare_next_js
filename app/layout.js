54675f5f5be9b6565197e2c55e9c9a04d7379594
IkeB108 <qwertyaeb@gmail.com>
2025-01-17 00:06:04 -0800

    d

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         {/* Load untar.js with a script tag */}
-        <script src="/untar.js"></script>
+        <script src="/sam_browser_test_bare_next_js/out/untar.js"></script>
       </head>
       <body>{children}</body>
     </html>
